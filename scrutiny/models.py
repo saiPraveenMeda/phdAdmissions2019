71a059c62cea72ab864104d7d7ef6d2e9cc6c8c9
= <=>
2017-10-06 20:20:49 +0530

    Fixed userprofile dup issue

--- scrutiny/models.py
+++ scrutiny/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
+class ScrutinyUserProfile(models.Model):
 	user = models.ForeignKey(User)
 	department = models.CharField(max_length=50)
 
