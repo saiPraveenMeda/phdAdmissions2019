73ec4dbca950a9c6c2941908faf75be082602661
= <=>
2017-10-07 00:38:24 +0530

    Department foreign key

--- scrutiny/models.py
+++ scrutiny/models.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from registration.models import Department
 
 class ScrutinyUserProfile(models.Model):
 	user = models.ForeignKey(User)
-	department = models.CharField(max_length=50)
+	department = models.ForeignKey(Department)
 
 	def __str__(self):
 		return (self.department+" - "+self.user.first_name+" "+self.user.last_name)
