cd0a26b73a1c100358d8c6050005b2eae93261fb
VamsiUCSS <ucssvamsi@gmail.com>
2017-10-14 23:23:35 +0530

    settings

--- facultyForm/settings.py
+++ facultyForm/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import confidential
-from database import DATABASES, EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
