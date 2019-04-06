b80d8c57df3b88c8ffd5101fb4a15f194095867a
saiPraveenMeda <saipraveenmeda@gmail.com>
2019-04-06 11:44:45 +0530

    urls comments removed

--- registration/urls.py
+++ registration/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
-import views
+from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    # url(r'^createApp/$',views.createApp,name='createApp'),
-    # url(r'^signup/(?P<errorNo>[0-9]{1})/$',views.send_to_register,name='send_to_register'),
-    # url(r'^signup/$',views.send_to_register,name='send_to_register'),
+    url(r'^createApp/$',views.createApp,name='createApp'),
+    url(r'^signup/(?P<errorNo>[0-9]{1})/$',views.send_to_register,name='send_to_register'),
+    url(r'^signup/$',views.send_to_register,name='send_to_register'),
     url(r'^signin/$',views.signin,name='signin'),
     url(r'^signout/$',views.signout,name='signout'),
-    # url(r'^termsandconditions/$',views.termsandconditions,name='termsandconditions'),
+    url(r'^termsandconditions/$',views.termsandconditions,name='termsandconditions'),
     url(r'^forgotPassword/$',views.forgotPassword,name='forgotPassword'),
-    # url(r'^paymentDetails/$',views.paymentDetails,name='paymentDetails'),
+    url(r'^paymentDetails/$',views.paymentDetails,name='paymentDetails'),
 ]
